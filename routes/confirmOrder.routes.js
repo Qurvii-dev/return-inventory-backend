75544e36e5f8a4ace357cfacc8e056ea22584692
Qurvii-dev <cataloging@qurvii.com>
2025-06-26 18:36:23 +0530

    changed get from post

--- routes/confirmOrder.routes.js
+++ routes/confirmOrder.routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.route("/confirm-orders").get(getConfirmOrders);
 router.route("/add-to-ship").post(addToShip);
-router.route("/shipped-orders").post(getShippedOrders);
+router.route("/shipped-orders").get(getShippedOrders);
 
 
 
