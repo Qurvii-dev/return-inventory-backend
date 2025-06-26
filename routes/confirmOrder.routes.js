3179e19f0bdf0483c47ccdf9f509d1f6ea38070c
Qurvii-dev <cataloging@qurvii.com>
2025-06-26 18:27:49 +0530

    shipped route added

--- routes/confirmOrder.routes.js
+++ routes/confirmOrder.routes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 router.route("/confirm-orders").get(getConfirmOrders);
 router.route("/add-to-ship").post(addToShip);
+router.route("/shipped-orders").post(getShippedOrders);
+
 
 
 module.exports = router;
