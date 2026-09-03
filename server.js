a2292dd53c5f611e311b7e0a7c90d2bfa191ad71
Qurvii-dev <cataloging@qurvii.com>
2026-09-03 12:26:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,9 @@ const PORT = process.env.PORT || 5000;
 // global middlewares 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: ['https://scanreturn3.netlify.app', 'http://localhost:5174', 'http://localhost:5173']
+}));
 
 
 // routes middlewares for return inventory management
